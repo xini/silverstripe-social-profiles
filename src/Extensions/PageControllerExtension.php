df20998bf097511a030b959359ab1238c0510348
Florian Thoma <florian.thoma@innoweb.com.au>
2018-06-13 14:25:07 +0200

    fix requirements inclusion path

--- src/Extensions/PageControllerExtension.php
+++ src/Extensions/PageControllerExtension.php
@@ -8,7 +8,7 @@ use SilverStripe\View\Requirements;
 class PageControllerExtension extends Extension {
 
 	public function onAfterInit() {
-	    Requirements::css('innoweb/silverstripe-social-share:css/social-profiles.css');
+	    Requirements::css('innoweb/silverstripe-social-profiles:/client/css/social-profiles.css');
 	}
 
 }
